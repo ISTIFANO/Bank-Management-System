52fd7fb7e49516aa057610aa0a8b6e4bc1c117d6
istifano <elamiriamir7@gmail.com>
2025-03-11 10:57:43 +0100

    j ai criee la methode qui criee une transaction avec deux utilisateur (CreateTransaction)

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Wallet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
-    //
+
+    public function CreateTransaction(Request $request)
+    {
+
+
+        $request->validate(["amount" => "required", "description" => "required", "name" => "required", "email" => "required"]);
+        $sender_id = Auth::user()->id();
+$sender = User::find($sender_id);
+        $sender_wallet =  Wallet::find($sender_id);
+
+if(!($sender_wallet->balance <= $request["amount"])){
+
+
+    return response()->json(["message" => "budjet not found"]);
+}
+
+
+        $user = User::where((['email' => $request["email"]]));
+        if (!$user) {
+
+            return response()->json(["message" => "user not found"]);
+        }
+
+        $wallet =  Wallet::find($user->id);
+        $budjet = $request['amount'] + $wallet->balance;
+        Wallet::update(['balance' => $budjet]);
+
+      
+        return response()->json(["reciever" => $user, "sender" => $sender, "amount" => $request['amount']]);
+    }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'amount',
+        'description',
+        'date',
+        'name',
+        'email'
+    ];
 }
