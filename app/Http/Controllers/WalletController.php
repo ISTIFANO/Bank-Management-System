39205c94890dfbc88998ace110179cae63b84626
istifano <elamiriamir7@gmail.com>
2025-03-12 16:13:05 +0100

    j ai criee la methode qui ajouter amount a balance de utilisateur et la methode de  delete transaction

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Wallet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WalletController extends Controller
 {
@@ -11,40 +12,36 @@ class WalletController extends Controller
 
 
 
-    public function createWallet(Request $request){
-        $request->validate(["number" => "required", "user_id" => "required","balance" => "required"]);
+  public function createWallet(Request $request)
+  {
+    $request->validate(["number" => "required", "user_id" => "required", "balance" => "required"]);
 
-        $request['number']=parent::RandomNb();
+    $request['number'] = parent::RandomNb();
 
-      $response =  Wallet::Create(['number'=>$request['number'],'user_id'=>$request['user_id'],'balance'=>$request['balance']]);
+    $response =  Wallet::Create(['number' => $request['number'], 'user_id' => $request['user_id'], 'balance' => $request['balance']]);
 
 
-        return response()->json($response,201);
-        }
+    return response()->json($response, 201);
+  }
 
-public function delete(Request $request){
+  public function delete(Request $request)
+  {
     $request->validate(["delete_id" => "required"]);
 
-  $wallet =  Wallet::find($request["delete_id"]);
+    $wallet =  Wallet::find($request["delete_id"]);
 
-$wallet->delete();
+    $wallet->delete();
 
-return response()->json(['message'=>'deleted']);
+    return response()->json(['message' => 'deleted']);
+  }
+  public function updateBalance(Request $request)
+  {
+    DB::table('wallets')
+      ->where('id', $request->id)
+      ->update([
+        'balance' => $request->balance
+      ]);
 
-}
-public function updateBalance(Request $request){
-    $request->validate(["delete_id" => "required"]);
-
-  $wallet =  Wallet::find($request["delete_id"]);
-
-$wallet->delete();
-
-return response()->json(['message'=>'deleted']);
-
-}
-
-
-
-
-
+    return response()->json(['message' => 'updated succ']);
+  }
 }
